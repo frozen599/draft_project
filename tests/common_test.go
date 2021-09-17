591fe1a2e1c6480b51a0e09179f93e2c392a9651
tuong <lovesunshine269@gmail.com>
2021-09-17 19:38:35 +0700

    Add new test

--- tests/common_test.go
+++ tests/common_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/gopher5889/interview/helper"
@@ -17,4 +18,18 @@ func TestLoadCustomer(t *testing.T) {
 	customers := helper.LoadCustomer("../data/customers.csv")
 	assert.NotNil(t, customers)
 	helper.PrintJSON(customers)
+}
+
+func TestSendMail(t *testing.T) {
+	config := helper.ParseConfig("../.env")
+	assert.NotNil(t, config)
+
+	from := "example@gmail.com"
+	recipients := []string{"nttuong58@gmail.com", "penguin147258369@gmail.com"}
+	var wg sync.WaitGroup
+	for _, r := range recipients {
+		wg.Add(1)
+		go helper.DoSendMail(from, r, "Test Message", config, &wg)
+	}
+	wg.Wait()
 }
