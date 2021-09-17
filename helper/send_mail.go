7ee2af7dabfea4084c086474d5b763390e08e870
tuong <lovesunshine269@gmail.com>
2021-09-17 15:59:47 +0700

    Add new helper

--- helper/send_mail.go
+++ helper/send_mail.go
@@ -1,7 +1,20 @@
 package helper
 
-import "github.com/gopher5889/interview/models"
+import (
+	"log"
+	"net/smtp"
 
-func SendMail(host, port, from, passwd string, customers []*models.Customer) {
+	"github.com/gopher5889/interview/models"
+)
 
+func SendMail(customer *models.Customer, host, port, from, passwd, msg string) {
+	// Authentication
+	auth := smtp.PlainAuth("", from, passwd, host)
+
+	//Sending email
+	err := smtp.SendMail(host+":"+port, auth, from, []string{customer.Email}, []byte(msg))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Send email successfully")
 }
